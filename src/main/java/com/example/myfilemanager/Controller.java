bb18bac3273ff50f03373dfe8cf349e1d6444434
Nai2knight7113 <naito.7113@gmail.com>
2024-04-19 23:39:02 +0900

    - デフォルトのタイトルバー削除 - アプリ内のタイトルバーをドラッグによるウィンドウ自体の移動を可能に

---
+++ src/main/java/com/example/myfilemanager/Controller.java
@@ -0,0 +1,48 @@
+package com.example.myfilemanager;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Controller {
+    // フィールド群
+    public Pane TitlePane;
+    public Pane MainPane;
+    public Button ExitButton;
+
+
+    // Xボタンの処理
+    @FXML
+    protected void onExitButtonClick(ActionEvent e) {
+        System.exit(0);
+    }
+
+    // ウィンドウ自体の移動
+    private double windowXOffset = 0;
+    private double windowYOffset = 0;
+
+    @FXML
+    protected void windowMovePressed(MouseEvent e) {
+        Node source = (Node) e.getSource();
+        windowXOffset = e.getScreenX() - source.getScene().getWindow().getX();
+        windowYOffset = e.getScreenY() - source.getScene().getWindow().getY();
+
+        System.out.println(windowXOffset + ", " + windowYOffset);
+    }
+
+    @FXML
+    protected void windowMoveDragged(MouseEvent e) {
+        Node source = (Node) e.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+        double x = e.getScreenX() - windowXOffset;
+        double y = e.getScreenY() - windowYOffset;
+
+        stage.setX(x);
+        stage.setY(y);
+    }
+
+}
--- src/main/java/com/example/myfilemanager/Main.java
+++ src/main/java/com/example/myfilemanager/Main.java
@@ -1,15 +1,18 @@
-package main;
+package com.example.myfilemanager;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        stage.initStyle(StageStyle.UNDECORATED);
+
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("ui.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
